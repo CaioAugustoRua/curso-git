27599241579d9ea52272ef13e42c67258e6e1ab5
Caio Augusto Rua <august18oliveira@gmail.com>
2020-08-10 18:19:47 -0300

    Exemplos iniciais de padrão ISO 8601

---
+++ Projeto_17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Projeto_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Parse("2000-02-25 13:05:58");
+            DateTime d2 = DateTime.Parse("2000-02-25T13:05:58Z");
+
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 Local: " + d1.ToLocalTime());
+            Console.WriteLine("d1 Utc: " + d1.ToUniversalTime());
+
+            \n
+
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 Utc: " + d2.ToUniversalTime());
+
+            Console.WriteLine();
+
+            Console.WriteLine(d2.ToString("yyy-MM-ddTHH:mm:ssZ")); // cuidado!
+            Console.WriteLine(d2.ToUniversalTime().ToString("yyy-MM-ddTHH:mm:ssZ"));
+        }
+    }
+}
