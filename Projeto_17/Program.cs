b7235dc91f5ad95dba3d1054ea6ac438fa568911
Caio Augusto Rua <august18oliveira@gmail.com>
2020-08-10 18:25:11 -0300

    Hello World! adicionado

--- Projeto_17/Program.cs
+++ Projeto_17/Program.cs
@@ -8,13 +8,13 @@ namespace Projeto_17
         {
             DateTime d1 = DateTime.Parse("2000-02-25 13:05:58");
             DateTime d2 = DateTime.Parse("2000-02-25T13:05:58Z");
-
+            
             Console.WriteLine("d1: " + d1);
             Console.WriteLine("d1 Kind: " + d1.Kind);
             Console.WriteLine("d1 Local: " + d1.ToLocalTime());
             Console.WriteLine("d1 Utc: " + d1.ToUniversalTime());
 
-            \n
+            Console.WriteLine();
 
             Console.WriteLine("d2: " + d2);
             Console.WriteLine("d2 Kind: " + d2.Kind);
@@ -25,6 +25,10 @@ namespace Projeto_17
 
             Console.WriteLine(d2.ToString("yyy-MM-ddTHH:mm:ssZ")); // cuidado!
             Console.WriteLine(d2.ToUniversalTime().ToString("yyy-MM-ddTHH:mm:ssZ"));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hello World!");
         }
     }
 }
